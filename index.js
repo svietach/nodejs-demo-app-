6b7d6d68eb80dae2fff7054d12b9c307f888f4e4
Svitlana Chaplinska <svitlana@Svitlanas-MacBook-Pro.local>
2020-05-16 15:12:17 +0300

    My first commit :tada:

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express'); // add all modules using require 
+const axios = require('axios');
+const server = express();
+const port = 3000;
+
+server.set('view engine', 'ejs');
+server.set('views', './views');
+
+server.use(express.static('public'));
+
+server.get('/', async (req, res) => {
+    const {hashtag} = req.query;
+
+    const response = await axios.get(encodeURI(`https://www.instagram.com/explore/tags/${hashtag}/?__a=1`));
+        res.render('index', {
+            items: response.data.graphql.hashtag.edge_hashtag_to_media.edges,
+         });
+}); //treak of function request 
+
+server.get('/name', (req, res) => {
+    const name = req.query.name;
+    res.render('index', {
+        name,
+      });
+ });
+server.listen(port, () =>
+{ console.log(`Example app listening at http://localhost:${port}`)}); //listening the port 
